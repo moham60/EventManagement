e7ca747ae3f1e892a199bfce7a26b9e0f7b6f48b
moham60 <alkingg289@gmail.com>
2025-09-04 00:36:00 +0300

    second

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -6,18 +6,13 @@ const venues=require('../seed/venues')
 
 const connectDB = () => {
     mongoose
-      .connect(
-        "mongodb+srv://mohamed:123456789Mm@cluster2.q0paitk.mongodb.net/EventX"
-        
-      )
-      .then(async() => {
+      .connect(process.env.MONGO_URI)
+      .then(async () => {
         console.log("✅ Connected to MongoDB");
-       for (const venue of venues) {
-         const exists = await Venue.findOne({ name: venue.name });
-         if (!exists) await Venue.create(venue);
-       }
-
-        
+        for (const venue of venues) {
+          const exists = await Venue.findOne({ name: venue.name });
+          if (!exists) await Venue.create(venue);
+        }
       })
       .catch((err) => {
         console.error("❌ MongoDB connection error:", err);
