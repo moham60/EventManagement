03c26a9559b048cba5aa504635258dfa165db3fa
moham60 <alkingg289@gmail.com>
2025-09-04 11:54:21 +0300

    lastAngm

--- Frontend/src/Components/BieChart.jsx
+++ Frontend/src/Components/BieChart.jsx
@@ -1,7 +1,10 @@
 import Chart from "react-apexcharts";
 
 export default function BieChart({
-  labels  ,title,height=300}) {
+  labels = ["Group A", "Group B", "Group C", "Group D"],
+  title,
+  height = 300,
+}) {
   const options = {
     chart: {
       type: "donut",
@@ -10,7 +13,7 @@ export default function BieChart({
       position: "bottom",
     },
     labels: labels,
-   
+
     plotOptions: {
       pie: {
         donut: {
@@ -20,18 +23,23 @@ export default function BieChart({
     },
     colors: ["#0DF38A", "#7224F2", "#2D44EC", "#FF371F"],
   };
-    const finalLabels =
-      labels.length > 0 ? labels : ["18-25", "Gr", "Group C", "Group D"];
-    const finalSeries = series.length > 0 ? series : [44, 55, 41, 67];
-
+  const finalLabels =
+    labels?.length > 0 ? labels : ["Group A", "Group B", "Group C", "Group D"];
+  const finalSeries = [44, 55, 41, 67];
 
   return (
     <div className="bg-white w-full  p-4 flex flex-col justify-center items-center rounded-2xl">
       <div className="title my-4">
-        <h3 className="font-bold uppercase">{ title}</h3>
+        <h3 className="font-bold uppercase">{title}</h3>
       </div>
       <div className="chart ">
-        <Chart options={options} series={series} type="donut" width="300" height={height} />
+        <Chart
+          options={options}
+          series={finalSeries}
+          type="donut"
+          width="300"
+          height={height}
+        />
       </div>
     </div>
   );
