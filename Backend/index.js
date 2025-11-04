0c093d36d48d3ba5686d41a955efca9b2a72753a
Mohamed Abdelwahab <105382862+moham60@users.noreply.github.com>
2025-11-05 01:37:24 +0200

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -47,7 +47,7 @@ app.use("/api/reports", reportsRoute);
 app.use("/api/user", userRoute);
 /////
 const PORT = process.env.PORT||5000;
-app.listen(PORT, () => {
-  console.log("🚀 Server running on port 5000");
-});
+module.exports = app;
+
+
 
